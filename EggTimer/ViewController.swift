888a7a7a69086db7d0b44c32bdb217e1cc3e6416
Alikhan Batchaev <loko-han@mail.ru>
2020-01-22 11:30:26 +0300

    добавил иконку, рефакторинг кода

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,42 +7,48 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var eggProgressBar: UIProgressView!
     
-    let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
+    var player: AVAudioPlayer!
     
-    var secondsRemaining = 60
-    
+    let eggTimes = ["Soft": 300, "Medium": 480, "Hard": 720]
     var timer = Timer()
+    var totalTime = 0
+    var secondPassed = 0
+    
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
         
-        eggProgressBar.progress = 1.0
-        
         timer.invalidate()
-        
         let hardness = sender.currentTitle!
+        totalTime = eggTimes[hardness]!
         
-        secondsRemaining = eggTimes[hardness]!
+        eggProgressBar.progress = 0.0
+        secondPassed = 0
+        titleLabel.text = hardness
         
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
         
     }
     
     @objc func updateCounter(){
-        if secondsRemaining > 0 {
-            print("\(secondsRemaining) seconds.")
-            secondsRemaining -= 1
+        if secondPassed < totalTime {
+            secondPassed += 1
+            eggProgressBar.progress = Float(secondPassed) / Float(totalTime)
         } else {
             timer.invalidate()
-            titleLabel.text = "Время вышло!"
+            titleLabel.text = "ГОТОВО!"
+            
+            let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player.play()
         }
     }
-    
 }
 
 
